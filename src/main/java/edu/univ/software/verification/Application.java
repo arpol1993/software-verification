0493523a6c02dee74fee4a8f0f6adba36819b151
Сергей <shutclare@ukr.net>
2015-06-08 02:41:39 +0300

    add example for product

--- src/main/java/edu/univ/software/verification/Application.java
+++ src/main/java/edu/univ/software/verification/Application.java
@@ -1,4 +1,3 @@
-
 package edu.univ.software.verification;
 
 import com.google.common.collect.ImmutableList;
@@ -8,12 +7,15 @@ import edu.univ.software.verification.model.KripkeStructure;
 import edu.univ.software.verification.model.MullerAutomaton;
 import edu.univ.software.verification.model.fa.BasicBuchiAutomaton;
 import edu.univ.software.verification.model.fa.BasicMullerAutomaton;
+import edu.univ.software.verification.model.fa.ProductsAutomatonClass;
 import edu.univ.software.verification.model.kripke.BasicStructure;
 import edu.univ.software.verification.model.ltl.Atom;
 import edu.univ.software.verification.model.ltl.BinaryOp;
 import edu.univ.software.verification.model.ltl.UnaryOp;
 import edu.univ.software.verification.utils.AutomataUtils;
+import edu.univ.software.verification.utils.DirectProduct;
 import edu.univ.software.verification.utils.LtlParser;
+import java.io.Serializable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +25,9 @@ import org.slf4j.LoggerFactory;
  * @author arthur
  */
 public class Application {
+
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
-    
+
     private static void kripkeStructureDemo() {
         // demonstrates creation of kripke model for p. 265
         KripkeStructure ks = BasicStructure.builder()
@@ -51,13 +54,13 @@ public class Application {
                 .withTransition("7", "2")
                 .withTransition("8", "1")
                 .build();
-        
+
         logger.info("---Kripke structure test---");
         logger.info("Edge between {} and {}: {}", 0, 2, ks.hasTransition("0", "2"));
         logger.info("Edge between {} and {}: {}", 3, 8, ks.hasTransition("3", "8"));
         logger.info("---------------------------");
     }
-    
+
     private static void buchiAutomatonDemo() {
         // demonstrates creation of Buchi automaton for p. 234a
         BuchiAutomaton<?> buchiAutomaton = BasicBuchiAutomaton.builder()
@@ -73,18 +76,51 @@ public class Application {
                 .withFinalState("3")
                 .withFinalState("4")
                 .build();
-        
+
         logger.info("---Buchi automaton test---");
         logger.info("Transition between {} and {}: {}", 2, 3, buchiAutomaton.hasTransition("2", "3"));
         logger.info("Transition between {} and {}: {}", 3, 1, buchiAutomaton.hasTransition("3", "1"));
         logger.info("Ingoing edges for state {}: {}", 4, buchiAutomaton.getTransitionsTo("4"));
-        logger.info("---------------------------");       
-        
+        logger.info("---------------------------");
+
         logger.info("Is that automaton accepts only empty language : {}",
                 AutomataUtils.INSTANCE.isEmptyLanguage(buchiAutomaton));
-         logger.info("---------------------------");      
+        logger.info("---------------------------");
     }
-    
+
+    private static void productBuchiAutomatonDemo() {
+
+        BuchiAutomaton<Serializable> buchiAutomatonA = BasicBuchiAutomaton.builder()
+                .withState("0", true)
+                .withStates("1")
+                .withTransition("0", "1", "x", "y")
+                .withTransition("1", "1", "y")
+                .withTransition("1", "0", "y")
+                .withFinalState("0")
+                .build();
+        BuchiAutomaton<Serializable> buchiAutomatonB = BasicBuchiAutomaton.builder()
+                .withState("0", true)
+                .withStates("1")
+                .withTransition("0", "1", "x", "y")
+                .withTransition("1", "1", "x", "y")
+                .withFinalState("1")
+                .build();
+
+        BuchiAutomaton<ProductsAutomatonClass> buchiAutomatonResult = DirectProduct.product(buchiAutomatonA, buchiAutomatonB);
+
+        logger.info("---Buchi product A automaton test---");
+        logger.info("States: {}", buchiAutomatonA.getStates());
+        logger.info("Transitions : {}", buchiAutomatonA.getTransitions());
+        logger.info("---Buchi product B automaton test---");
+        logger.info("States: {}", buchiAutomatonB.getStates());
+        logger.info("Transitions : {}", buchiAutomatonB.getTransitions());
+        logger.info("---Buchi product automaton test---");
+        logger.info("States: {}", buchiAutomatonResult.getStates());
+        logger.info("Transitions : {}", buchiAutomatonResult.getTransitions());
+        logger.info("Final : {}", buchiAutomatonResult.getFinalStates());
+
+    }
+
     private static void mullerAutomatonDemo() {
         // demonstrates creation of Muller automaton for p. 253
         MullerAutomaton<?> mullerAutomaton = BasicMullerAutomaton.builder()
@@ -97,15 +133,15 @@ public class Application {
                 .withFinalStateSet("1")
                 .withFinalStateSet("0")
                 .build();
-        
+
         logger.info("---Muller automaton test---");
         logger.info("Transition between {} and {}: {}", 1, 0, mullerAutomaton.hasTransition("1", "0"));
         logger.info("Transition between {} and {}: {}", 2, 1, mullerAutomaton.hasTransition("2", "1"));
-        logger.info("Outgoing edges for state {}: {}", 0, mullerAutomaton.getTransitionsFrom("0"));        
+        logger.info("Outgoing edges for state {}: {}", 0, mullerAutomaton.getTransitionsFrom("0"));
         logger.info("---------------------------");
-                
+
         BuchiAutomaton<?> buchiAutomaton = AutomataUtils.INSTANCE.convert(mullerAutomaton);
-                
+
         logger.info("---Degeneralization (LGBA -> BA) automaton test---");
         logger.info("Transition between {} and {}: {}", 1, 0, buchiAutomaton.hasTransition("(1, 0)", "(0, 1)"));
         logger.info("Transition between {} and {}: {}", 2, 1, buchiAutomaton.hasTransition("(2, 0)", "(1, 0)"));
@@ -114,22 +150,22 @@ public class Application {
         logger.info("Outgoing edges for state {}: {}", "(0, 0)", buchiAutomaton.getTransitionsFrom("(0, 0)"));
         logger.info("Outgoing edges for state {}: {}", "(1, 1)", buchiAutomaton.getTransitionsFrom("(1, 1)"));
         logger.info("---------------------------");
-        
-        
+
         logger.info("Is that automaton accepts only empty language : {}", AutomataUtils.INSTANCE.isEmptyLanguage(buchiAutomaton));
     }
-        private static void kripkeStructureToBuchiAutomataDemo(){
+
+    private static void kripkeStructureToBuchiAutomataDemo() {
         // demonstrates convertation (kripke model --> Buchi automaton) for p. 270
         KripkeStructure ks = BasicStructure.builder()
                 .withState("0", ImmutableList.of(Atom.forName("p")), true)
-                .withState("1", ImmutableList.of(Atom.forName("p"), Atom.forName("q")),true)
+                .withState("1", ImmutableList.of(Atom.forName("p"), Atom.forName("q")), true)
                 .withState("2", ImmutableList.of(Atom.forName("q")))
                 .withTransition("0", "1")
                 .withTransition("1", "0")
                 .withTransition("2", "0")
                 .withTransition("1", "2")
                 .build();
-        
+
         BuchiAutomaton buchiAutomaton = AutomataUtils.INSTANCE.convert(ks);
         logger.info("---Kripke model --> Buchi automaton test---");
         logger.info("Transition between {} and {}: {}", 0, 1, buchiAutomaton.hasTransition("0", "1"));
@@ -148,35 +184,36 @@ public class Application {
         logger.info("Transition formula between {}: is {}", "(3, 1)", buchiAutomaton.getTransitionSymbols("3", "1"));
         logger.info("---------------------------");
     }
-        
+
     private static void ltlDemo() {
         Atom atomB = new Atom("boss");
         Atom atomA = new Atom("_aAa");
         Atom atomZero = Atom._0;
         Atom atomOne = Atom._1;
-        BinaryOp implication = BinaryOp.build(BinaryOp.OpType.IMPL,atomA,atomB);
-        BinaryOp union = BinaryOp.build(BinaryOp.OpType.OR,atomZero,atomOne);
-        BinaryOp release = BinaryOp.build(BinaryOp.OpType.R,implication,union);
-        UnaryOp f = UnaryOp.build(UnaryOp.OpType.F,release);
-        
+        BinaryOp implication = BinaryOp.build(BinaryOp.OpType.IMPL, atomA, atomB);
+        BinaryOp union = BinaryOp.build(BinaryOp.OpType.OR, atomZero, atomOne);
+        BinaryOp release = BinaryOp.build(BinaryOp.OpType.R, implication, union);
+        UnaryOp f = UnaryOp.build(UnaryOp.OpType.F, release);
+
         logger.info("---LTL invertion test---");
         logger.info("Formula: {}", f);
         logger.info("Inverted: {}", f.invert());
         logger.info("Inverted & normalized: {}", f.invert().normalized());
         logger.info("------------------------");
-        
+
         logger.info("---LTL parser test------");
-        logger.info("Formula(REP): {}", LtlParser.parseString(f.toString()));        
+        logger.info("Formula(REP): {}", LtlParser.parseString(f.toString()));
         logger.info("Inverted(REP): {}", LtlParser.parseString(f.invert().toString()));
         logger.info("Inv. & Norm.(REP): {}", LtlParser.parseString(f.invert().normalized().toString()));
         logger.info("------------------------");
     }
-    
+
     public static void main(String[] args) {
         kripkeStructureDemo();
         ltlDemo();
         buchiAutomatonDemo();
         mullerAutomatonDemo();
         kripkeStructureToBuchiAutomataDemo();
+        productBuchiAutomatonDemo();
     }
 }
